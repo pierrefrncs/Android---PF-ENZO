6e909cdb528471dc47b7f25edbb84ba50b14392e
Nzolo0 <55888232+Nzolo0@users.noreply.github.com>
2020-06-06 16:53:36 +0200

    Fix bugs + optimisations

--- android/app/src/main/java/app/epf/ratp_eb_pf/ui/favoris/FavorisStationsFragment.kt
+++ android/app/src/main/java/app/epf/ratp_eb_pf/ui/favoris/FavorisStationsFragment.kt
@@ -183,56 +183,59 @@ class FavorisStationsFragment : Fragment() {
                 actionState: Int,
                 isCurrentlyActive: Boolean
             ) {
+                if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
 
-                val itemView = viewHolder.itemView
-                val itemHeight = itemView.bottom - itemView.top
-                val isCanceled = dX == 0f && !isCurrentlyActive
+                    val itemView = viewHolder.itemView
+                    val itemHeight = itemView.bottom - itemView.top
+                    val isCanceled = dX == 0f && !isCurrentlyActive
 
-                if (isCanceled) {
-                    clearCanvas(
-                        c,
-                        itemView.right + dX,
-                        itemView.top.toFloat(),
-                        itemView.right.toFloat(),
-                        itemView.bottom.toFloat()
+                    if (isCanceled) {
+                        clearCanvas(
+                            c,
+                            itemView.right + dX,
+                            itemView.top.toFloat(),
+                            itemView.right.toFloat(),
+                            itemView.bottom.toFloat()
+                        )
+                        super.onChildDraw(
+                            c,
+                            recyclerView,
+                            viewHolder,
+                            dX,
+                            dY,
+                            actionState,
+                            isCurrentlyActive
+                        )
+                        return
+                    }
+
+                    // Draw the red delete background
+                    background.color = backgroundColor
+                    background.setBounds(
+                        itemView.right + dX.toInt(),
+                        itemView.top,
+                        itemView.right,
+                        itemView.bottom
                     )
-                    super.onChildDraw(
-                        c,
-                        recyclerView,
-                        viewHolder,
-                        dX,
-                        dY,
-                        actionState,
-                        isCurrentlyActive
+                    background.draw(c)
+
+                    // Calculate position of delete icon
+                    val deleteIconTop = itemView.top + (itemHeight - intrinsicHeight) / 2
+                    val deleteIconMargin = (itemHeight - intrinsicHeight) / 2
+                    val deleteIconLeft = itemView.right - deleteIconMargin - intrinsicWidth
+                    val deleteIconRight = itemView.right - deleteIconMargin
+                    val deleteIconBottom = deleteIconTop + intrinsicHeight
+
+                    // Draw the delete icon
+                    deleteIcon?.setBounds(
+                        deleteIconLeft,
+                        deleteIconTop,
+                        deleteIconRight,
+                        deleteIconBottom
                     )
-                    return
+                    deleteIcon?.draw(c)
+
                 }
-
-                // Draw the red delete background
-                background.color = backgroundColor
-                background.setBounds(
-                    itemView.right + dX.toInt(),
-                    itemView.top,
-                    itemView.right,
-                    itemView.bottom
-                )
-                background.draw(c)
-
-                // Calculate position of delete icon
-                val deleteIconTop = itemView.top + (itemHeight - intrinsicHeight) / 2
-                val deleteIconMargin = (itemHeight - intrinsicHeight) / 2
-                val deleteIconLeft = itemView.right - deleteIconMargin - intrinsicWidth
-                val deleteIconRight = itemView.right - deleteIconMargin
-                val deleteIconBottom = deleteIconTop + intrinsicHeight
-
-                // Draw the delete icon
-                deleteIcon?.setBounds(
-                    deleteIconLeft,
-                    deleteIconTop,
-                    deleteIconRight,
-                    deleteIconBottom
-                )
-                deleteIcon?.draw(c)
 
                 super.onChildDraw(
                     c,
@@ -243,6 +246,8 @@ class FavorisStationsFragment : Fragment() {
                     actionState,
                     isCurrentlyActive
                 )
+
+
             }
 
             private fun clearCanvas(
